bcb511dbafd252987858d80a031a60f4e9aa42fa
Alen Murtić <murtic.alen@hotmail.com>
2024-03-05 13:53:50 +0100

    [Week:02] Add lesson and homework (#2)

---
+++ Lessons/02/asi.js
@@ -0,0 +1,14 @@
+1 + 1 // no semicolon, try adding one
+-1 + 1 === 0 ? console.log(0) : console.log(2)
+
+const v = {
+}
+
+console.log(v)
+
+const v1 = new Object()
+console.log(v1)
+
+
+const v2 = Object.create({})
+console.log(v2)

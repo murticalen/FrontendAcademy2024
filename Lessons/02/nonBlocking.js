d2db841517ce4b7623bf9da8db7e23fc9132dbaf
Alen Murtić <murtic.alen@hotmail.com>
2024-03-12 21:32:35 +0100

    [Week:02] Update lesson 2 after the actual lesson

--- Lessons/02/nonBlocking.js
+++
@@ -1,8 +0,0 @@
-console.log('Start')
-
-setTimeout(() => {
-  console.log('Callback')
-  // what happens if we set timeout to 0s -> Should console.log happen immediately?
-}, 1000)
-
-console.log('End')

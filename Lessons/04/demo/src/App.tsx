7fbcb649ee718cffde2e32476e4f47ac5e8d1974
Alen Murtić <murtic.alen@hotmail.com>
2024-03-20 09:33:28 +0100

    [Week:04] Add lesson 04 examples

---
+++ Lessons/04/demo/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+import { Clock } from './Clock'
+import { Counter } from './Counter'
+import { Fetch } from './Fetch'
+import { HelloWorld } from './HelloWorld'
+
+function App() {
+
+  const times = 5
+  const [showClock, setShowClock] = useState(false)
+
+  return (
+    <>
+      <HelloWorld color='orange' transform="uppercase" times={times}>
+        <div>Bla bla bla</div>
+        <div>Test test test</div>
+        </HelloWorld>
+        <Counter/>
+        <Counter/>
+        <Fetch/>
+        <div style={{cursor: 'pointer'}} onClick={() => setShowClock(v => !v)}>{showClock ? 'HIDE CLOCK' : 'SHOW CLOCK'}</div>
+        {showClock && <Clock/>}
+    </>
+  )
+}
+
+export default App

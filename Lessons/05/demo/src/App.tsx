3c3bd0c75120e92c7deca759ede7cb4bbeab19b6
Alen Murtić <murtic.alen@hotmail.com>
2024-03-26 15:38:11 +0100

    remove theme setter for now

--- Lessons/05/demo/src/App.tsx
+++ Lessons/05/demo/src/App.tsx
@@ -1,14 +1,13 @@
 import './App.css'
-import { ThemeSetter } from './ThemeSetter.tsx'
 
 function App() {
 
   return (
-    <ThemeSetter>
+    <div className="light-theme">
       <div className="app-header">
         Your App:
       </div>
-    </ThemeSetter>
+    </div>
   )
 }
 

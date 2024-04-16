74c5e0639e26da68da53f6d43a6e5f5a44f05270
Alen Murtić <murtic.alen@hotmail.com>
2024-04-16 19:35:46 +0200

    part2 progress

--- Lessons/next-app/src/modules/Weather/component/CitiesWeather.tsx
+++ Lessons/next-app/src/modules/Weather/component/CitiesWeather.tsx
@@ -1,7 +1,35 @@
+import { CityData } from "../interface"
 import { CityCard } from "./CityCard"
+
+const data: CityData[] = [
+    {
+        name: "Zagreb",
+        location: {
+            position: "center",
+            distance: 0
+        },
+        weather: {
+            temp: 9,
+            type: "rainy"
+        }
+    },
+    {
+        name: "San Francisco",
+        location: {
+            position: "California",
+            distance: 9999
+        },
+        weather: {
+            temp: 35,
+            type: "sunny"
+        }
+    }
+]
 
 export const CitiesWeather = () => {
     return (
-        <CityCard city={{name: 'Zagreb', location: {position: 'center', distance: 0}, weather: {temp: 9, type: 'rainy'}}}/>
+        <>
+        {data.map((city, index) => <CityCard key={city.name} city={city} mx="spacings.sm" mt={index === 0 ? undefined : 'spacings.md'}/>)}
+        </>
     )
 }
--- Lessons/next-app/src/modules/Weather/component/CityCard.tsx
+++ Lessons/next-app/src/modules/Weather/component/CityCard.tsx
@@ -1,22 +1,30 @@
-import { Box, BoxProps, Flex, Text } from "@kuma-ui/core"
+import { Box, BoxProps, Flex, FlexProps, Text } from "@kuma-ui/core"
 import { CityData, CityLocation, CityWeather } from "../interface"
 
 const CardHeader = ({city}: {city: CityData}) => {
     return (
-        <Flex gap="spacings.lg" justify="space-between" h="24px">
+        <Flex gap="spacings.lg" justify="space-between" alignItems="center" h="24px">
             <Text fontSize="20px" color="colors.onSurface.nLv1">{city.name}</Text>
             <Box w="24px" h="24px" color="colors.secondary">S</Box>
         </Flex>
     )
 }
 
-const CardContent = ({city}: {city: CityData}) => {
-    return (<Flex><Location location={city.location}/><Weather weather={city.weather}/></Flex>)
+const CardContent = ({city, ...rest}: {city: CityData} & FlexProps) => {
+    return (
+        <Flex justify={["space-between", 'space-evenly', 'space-between']} _hover={{backgroundColor: 'aquamarine',  cursor: "pointer"}} {...rest}>
+            <Location location={city.location}/>
+            <Weather weather={city.weather}/>
+        </Flex>
+    )
 }
 
 const Location = ({location}: {location: CityLocation}) => {
     return (
-        <Box>sdfjsdfksdkl</Box>
+        <Box color="colors.onSurface.nLv2">
+            <Text>{location.position}</Text>
+            <Text>Distance: {location.distance} km</Text>
+        </Box>
     )
 }
 
@@ -28,9 +36,9 @@ const Weather = ({weather}: {weather: CityWeather}) => {
 
 export const CityCard = ({city, ...rest}: {city: CityData} & BoxProps) => {
     return (
-        <Box bg="colors.surface.s1" minH="120px" pt="spacings.lg" px="spacings.lg" mx="spacings.sm" borderRadius="spacings.md" {...rest}>
+        <Box bg="colors.surface.s1" minH="120px" pt="spacings.lg" px="spacings.lg" borderRadius="spacings.md" {...rest}>
             <CardHeader city={city}/>
-            <CardContent city={city}/>
+            <CardContent city={city} mt="20px"/>
         </Box>
     )
 }

7b3c29de59f482a3d560cddd0253504c77c13c8e
Alen Murtić <murtic.alen@hotmail.com>
2024-04-16 11:32:20 +0200

    add kuma ui dep

--- Lessons/next-app/next.config.mjs
+++ Lessons/next-app/next.config.mjs
@@ -1,6 +1,8 @@
+import { withKumaUI } from "@kuma-ui/next-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
 };
 
-export default nextConfig;
+export default withKumaUI(nextConfig);

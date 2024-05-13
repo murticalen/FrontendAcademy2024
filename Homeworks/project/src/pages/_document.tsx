2c16f7e8589acf5880ddf8bf0040c5f7aeba0840
Alen Murtić <murtic.alen@hotmail.com>
2024-05-13 15:01:01 +0200

    one source of truth

--- Homeworks/project/src/pages/_document.tsx
+++ Homeworks/project/src/pages/_document.tsx
@@ -30,7 +30,10 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
         <body>
         <Main />
         <NextScript />
